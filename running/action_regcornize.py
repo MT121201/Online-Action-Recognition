10888dbb494012441d10ae2d3c4c42356cf8e678
MT121201 <triet.kieu_binu@hcmut.edu.vn>
2023-08-31 14:41:20 +0700

    test pipeline oad

---
+++ running/action_regcornize.py
@@ -0,0 +1,90 @@
+import cv2
+import numpy as np
+from collections import deque
+import onnxruntime as ort
+from torchvision import models, datasets, transforms as T
+import torch
+from PIL import Image
+class Regcornize_action:
+    def __init__ (self, model_path, super_image_size, threshold=0.5):
+        self.model_path = model_path
+        self.super_image_size = super_image_size
+        self.threshold = threshold
+        self.queue = deque(maxlen=super_image_size[0]*super_image_size[1])
+        self.preprocess = T.Compose([
+        T.Resize(256),
+        T.CenterCrop(224),
+        T.ToTensor(),
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+        # build model
+        providers = [
+            'CUDAExecutionProvider'
+        ]
+        self.sess = ort.InferenceSession(self.model_path, providers=providers)
+    
+    def crop_detect_images(self, img, result):
+        # crop image
+        x1, y1, x2, y2 = result[:4]
+        x1 = max(0, int(x1))
+        y1 = max(0, int(y1))
+        x2 = min(img.shape[1], int(x2))
+        y2 = min(img.shape[0], int(y2))
+        crop_img = img[y1:y2, x1:x2]
+        return crop_img
+    
+    def resize_image(self, img, height=224, width=224):
+        # resize and expand batch dimension
+        img = cv2.resize(img, (width, height))
+        return img
+    
+    def update_queue(self, img):
+        # update queue
+        if len(self.queue) < self.super_image_size[0]*self.super_image_size[1]:
+            self.queue.append(img)
+            return False
+        else:
+            self.queue.popleft()
+            self.queue.append(img)
+            return True
+    def super_image(self):
+        # make super image 3x3 from queue
+        queue_array = np.array(self.queue)
+        super_image = queue_array.reshape(self.super_image_size[0], self.super_image_size[1], 224, 224, 3).swapaxes(1, 2).reshape(3*224, 3*224, 3)
+        return super_image
+    
+    def predict(self, img):
+        #convert to PIL
+        img = Image.fromarray(img)
+        input_tensor = self.preprocess(img)
+        input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+        if torch.cuda.is_available():
+            input_batch = input_batch.to('cuda')
+        else:
+            raise Exception("Cuda is not available")
+        # predict
+        with torch.no_grad():
+            output = self.sess.run(None, {'input': input_batch.cpu().numpy()})[0]
+        return output
+    
+    def run(self, img, result):
+        # crop image
+        crop_img = self.crop_detect_images(img, result)
+        # resize image
+        resize_img = self.resize_image(crop_img)
+        # update queue
+        full_queue = self.update_queue(resize_img)
+        # # if queue is full
+        if full_queue:
+            # make super image
+            super_image = self.super_image()
+            # predict
+            output = self.predict(super_image)
+            # return if output score > threshold
+            if np.max(output) > self.threshold:
+                return np.argmax(output)
+            else:
+                return None
+            
+        else:
+            return None
---
+++ running/oad_pipeline.py
@@ -0,0 +1,55 @@
+from action_regcornize import Regcornize_action
+from detection import Detection_model
+
+import cv2
+import numpy as np
+
+from tqdm import tqdm
+def draw_bbox(img, bbox, predict, color=(0, 255, 0), thickness=2):
+    x1, y1, x2, y2 = bbox[:4]
+    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    # import pdb; pdb.set_trace()
+    cv2.rectangle(img, (x1, y1), (x2, y2), color, thickness)
+    cv2.putText(img, str(predict), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, color, thickness=2)
+    save_path = './cache/frame.jpg'
+    cv2.imwrite(save_path, img)
+    return img
+
+def main():
+    detection_model = Detection_model(model_path="/models/person/yolox_s_hdet_vga_60e_onnxonly_1280x800.onnx", H=800, W=1280, save_path=None)
+    regcornize_action = Regcornize_action(model_path="/home/tni/Workspace/triet/Vehicle-Classification/onnx_model/action_regcornize.onnx", super_image_size=(3,3), threshold=0.5)
+    input_video = "/data/its/oad/triet_test/video/call_test.mp4"
+    save_path = "./cache/video/call_test.mp4"
+    class_name = ("call", "read", "purcharse", "read")
+#   read and write video
+    cap = cv2.VideoCapture(input_video)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(save_path, fourcc, fps, (width, height))
+    #pbar
+    pbar = tqdm(total=int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            # detection
+            results = detection_model.detect(frame)
+            for result in results:
+                # regcornize
+                predict = regcornize_action.run(frame, result)
+                # # draw bbox
+                if predict is not None:
+                    frame = draw_bbox(frame, result, class_name[predict])
+            out.write(frame)
+            pbar.update(1)
+        else:
+            break
+    
+    cap.release()
+    out.release()
+    print("Done")
+    pbar.close()
+if __name__ == "__main__":
+    main()
+        
